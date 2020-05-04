51074736b6d9348a2853e2de831a43b3b1762791
blissGenSTLT <bliss.gen.93@gmail.com>
2020-05-03 19:00:49 -0700

    new

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key:         'WCj1t9S2RSZN4850vWkgnDAxI',
-    consumer_secret:      'BXLADJsJe0hF6HAXUULRZlGViq3Zh0i80vwGdkGAzzw0d904dC',
-    access_token:         '1232135222952484864-yPP2u9iqxCNfktj10ONNDNi4pF553U',
-    access_token_secret:  '7Hlv0JiZK3EYQgt5OwNta9WCaCWHef0vZ194CcvEvgSsG'
-}
